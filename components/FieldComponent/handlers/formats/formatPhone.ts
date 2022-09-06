72d8f4d057ab834021a170232a4021f66fd485de
Alex Flok <alexflok2018@gmail.com>
2022-09-06 00:44:44 -0400

    added package file

---
+++ components/FieldComponent/handlers/formats/formatPhone.ts
@@ -0,0 +1,16 @@
+const formatPhone = (value: string) => {
+  let newValue: string = value;
+
+  if (value.length > 0 && value[0] !== "(") {
+    newValue = `(${value}`;
+  }
+  if (value.length > 3 && value[4] !== ")")
+    newValue = `${value.slice(0, 4)}) - ${value.slice(4, value.length)}`;
+
+  if (value.length > 10 && value[12] !== "-")
+    newValue = `${value.slice(0, 11)} - ${value.slice(11, value.length)}`;
+
+  return newValue;
+};
+
+export default formatPhone;
